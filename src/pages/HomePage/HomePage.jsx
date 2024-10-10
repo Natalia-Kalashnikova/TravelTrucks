23a64021538f85dea928fb04aaf1c589781372f9
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-10-10 21:18:20 +0300

    Add base css

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,12 @@
+import sprite from "../../images/icon.svg";
+
 const HomePage=()=>{
     return(
         <div>
             <h1>Welcome to the Home Page</h1>
+            <svg >
+                <use xlinkHref={`${sprite}#icon-gas`}></use>
+              </svg>
         </div>
     );    
 }
