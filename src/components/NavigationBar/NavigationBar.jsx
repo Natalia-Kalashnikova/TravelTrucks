f2be2dce03f342ab2a926e248a553b7b07f5daa3
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-10-12 01:42:49 +0300

    Update NavigationBar component

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,23 +1,52 @@
+// import css from './NavigationBar.module.css';
+// import { NavLink } from "react-router-dom";
+// import photo from '../../images/Logo.png';
+// import photo2x from '../../images/Logo.png';
+
+// const NavigationBar =()=>{
+//     return(
+//         <div className={css.navigationBarContainer}>            
+//             <picture className={css.logo}>
+//                 <source srcSet={`${photo2x} 2x`} />
+//                 <img className={css.imgLogo} src={photo} alt="logo" />
+//             </picture>
+//             <NavLink className={({isActive})=> 
+//                 isActive ? css.activeLink : css.link} to="/" end>
+//                     Home
+//             </NavLink>
+//             <NavLink className={({isActive})=> 
+//                 isActive ? css.activeLink : css.link} to="/catalog" end>
+//                     Catalog
+//             </NavLink>
+//         </div>
+//     );
+// }
+
+// export default NavigationBar;
+
+
 import css from './NavigationBar.module.css';
 import { NavLink } from "react-router-dom";
 import photo from '../../images/Logo.png';
-import photo2x from '../../images/Logo.png';
+import photo2x from '../../images/Logo@2x.png';
 
 const NavigationBar =()=>{
     return(
-        <div className={css.navigationBarContainer}>
-            <picture>
-                <source srcSet={`${photo2x} 2x`} />
-                <img className={css.imgLogo} src={photo} alt="logo" />
-            </picture>
-            <NavLink className={({isActive})=> 
-                isActive ? css.activLink : css.link} to="/" end>
-                    Home
-            </NavLink>
-            <NavLink className={({isActive})=> 
-                isActive ? css.activLink : css.link} to="/catalog" end>
-                    Catalog
-            </NavLink>
+        <div className={css.header}>
+            <div className={css.navigationBarContainer} >
+                <picture className={css.logo}>
+                    <source srcSet={`${photo2x} 2x`} />
+                    <img className={css.imgLogo} src={photo} alt="logo" />
+                </picture>
+                <NavLink className={({isActive})=> 
+                    isActive ? css.activeLink : css.link} to="/" end>
+                        Home
+                </NavLink>
+                <NavLink className={({isActive})=> 
+                    isActive ? css.activeLink : css.link} to="/catalog" end>
+                        Catalog
+                </NavLink>
+            </div>            
         </div>
     );
 }
