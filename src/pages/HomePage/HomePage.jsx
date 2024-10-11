8a6baae88d60c378b2a7047e5b4775e3c8c6d7c3
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-10-12 01:48:13 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage=()=>{
     return <div className={css.homePageContainer}>
         <h1 className={css.homePageTitle}>Campers of your dreams</h1>
         <p className={css.homePageText}>You can find everything you want in our catalog</p>
-        <Link className={css.nvgBtn}to="/catalog">View Now</Link>
+        <Link className={css.navBtn}to="/catalog">View Now</Link>
     </div>
 }
 
